76f27f37d4533ac2da043c00cbef658f7aecc272
Sayyad Shekshavali <syedshaikshavali330@gmail.com>
2025-04-30 11:13:59 +0530

    Update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -7,7 +7,7 @@ const connectToDb = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MDB_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
